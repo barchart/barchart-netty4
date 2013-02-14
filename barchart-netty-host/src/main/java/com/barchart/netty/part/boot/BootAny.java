39b835c52702f65df9505cc727c54e3df2d86512
jjongsma <jeremy@barchart.com>
2013-02-14 15:36:29 -0600

    Updated pipeline timeout implementation

--- barchart-netty-host/src/main/java/com/barchart/netty/part/boot/BootAny.java
+++ barchart-netty-host/src/main/java/com/barchart/netty/part/boot/BootAny.java
@@ -22,7 +22,6 @@ import com.barchart.netty.util.point.NetPoint;
 public abstract class BootAny implements NettyBoot {
 
 	protected final Logger log = LoggerFactory.getLogger(getClass());
-	protected final static String PIPELINE_TIMEOUT = "pipeline-timeout";
 
 	@Activate
 	protected void bootActivate() {
@@ -59,9 +58,9 @@ public abstract class BootAny implements NettyBoot {
 				/** always link channel with owner end point */
 				// channel.attr(NettyDot.ATTR_NET_POINT).set(netPoint);
 
-				final int timeout = netPoint.getInt(PIPELINE_TIMEOUT, 10000);
 				final NettyPipe pipe =
-						pipeManager().findPipe(netPoint.getPipeline(), timeout,
+						pipeManager().findPipe(netPoint.getPipeline(),
+								netPoint.getPipelineTimeout(),
 								TimeUnit.MILLISECONDS);
 
 				if (pipe == null) {
