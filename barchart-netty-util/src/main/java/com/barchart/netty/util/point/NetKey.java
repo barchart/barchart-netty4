694f9288b5dd38709b1cd4d7178876850c806901
jjongsma <jeremy@barchart.com>
2013-02-14 15:30:29 -0600

    Added pipeline timeout

--- barchart-netty-util/src/main/java/com/barchart/netty/util/point/NetKey.java
+++ barchart-netty-util/src/main/java/com/barchart/netty/util/point/NetKey.java
@@ -11,6 +11,9 @@ public interface NetKey {
 	/** pipeline name (pipe component name ) */
 	String KEY_PIPELINE = "pipeline";
 
+	/** Time to wait for the pipeline to appear via OSGI injection */
+	String KEY_PIPELINE_TIMEOUT = "pipeline-timeout";
+
 	String KEY_LOCAL_ADDRESS = "local-address";
 
 	String KEY_REMOTE_ADDRESS = "remote-address";
