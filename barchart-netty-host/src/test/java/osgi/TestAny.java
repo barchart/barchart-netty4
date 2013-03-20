923b6800caf84ad6511377d895746e46a0cd5a99
jjongsma <jeremy@barchart.com>
2013-03-20 17:16:42 -0500

    Updated

--- barchart-netty-host/src/test/java/osgi/TestAny.java
+++ barchart-netty-host/src/test/java/osgi/TestAny.java
@@ -7,8 +7,15 @@
  */
 package osgi;
 
-import static org.junit.Assert.*;
-import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.bundle;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemPackage;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.systemTimeout;
+import static org.ops4j.pax.exam.CoreOptions.wrappedBundle;
 
 import java.net.URL;
 import java.util.Dictionary;
@@ -73,7 +80,7 @@ public class TestAny {
 				//
 
 				wrappedBundle(mavenBundle().groupId("io.netty")
-						.artifactId("netty").versionAsInProject()),
+						.artifactId("netty-all").versionAsInProject()),
 
 				//
 
@@ -157,7 +164,8 @@ public class TestAny {
 	}
 
 	private static final String PAX_PID = "org.ops4j.pax.logging";
-	private static final String PAX_SERVICE = "org.ops4j.pax.logging.PaxLoggingService";
+	private static final String PAX_SERVICE =
+			"org.ops4j.pax.logging.PaxLoggingService";
 
 	private void loggingActivate(final ConfigurationAdmin configAdmin)
 			throws Exception {
@@ -168,13 +176,13 @@ public class TestAny {
 
 		props.load(propsURL.openStream());
 
-		final org.osgi.service.cm.Configuration config = configAdmin
-				.getConfiguration(PAX_PID, null);
+		final org.osgi.service.cm.Configuration config =
+				configAdmin.getConfiguration(PAX_PID, null);
 
 		config.update(props);
 
-		final ServiceTracker tracker = new ServiceTracker(context, PAX_SERVICE,
-				null);
+		final ServiceTracker tracker =
+				new ServiceTracker(context, PAX_SERVICE, null);
 
 		tracker.open(true);
 
@@ -189,8 +197,8 @@ public class TestAny {
 	private void loggingDeactivate(final ConfigurationAdmin configAdmin)
 			throws Exception {
 
-		final org.osgi.service.cm.Configuration config = configAdmin
-				.getConfiguration(PAX_PID, null);
+		final org.osgi.service.cm.Configuration config =
+				configAdmin.getConfiguration(PAX_PID, null);
 
 		config.delete();
 
@@ -202,7 +210,8 @@ public class TestAny {
 
 		final String name = EventHandler.class.getName();
 
-		final Dictionary<String, Object> props = new Hashtable<String, Object>();
+		final Dictionary<String, Object> props =
+				new Hashtable<String, Object>();
 		props.put(EventConstants.EVENT_TOPIC, topic);
 
 		context.registerService(name, this, props);
