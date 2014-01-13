51eaf42f792f5d01fb12c64a511c51c86704f1ca
jjongsma <jeremy@barchart.com>
2014-01-13 09:01:35 -0600

    Added default SCTP option

--- server/src/main/java/com/barchart/netty/server/stream/SCTPServer.java
+++ server/src/main/java/com/barchart/netty/server/stream/SCTPServer.java
@@ -1,6 +1,8 @@
 package com.barchart.netty.server.stream;
 
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.sctp.SctpChannelOption;
 import io.netty.channel.sctp.nio.NioSctpServerChannel;
 
 import java.net.SocketAddress;
@@ -11,6 +13,12 @@ public class SCTPServer extends AbstractStatefulServer<SCTPServer> {
 
 	public SCTPServer() {
 		channel(NioSctpServerChannel.class);
+	}
+
+	@Override
+	public ServerBootstrap bootstrap() {
+		return super.bootstrap().childOption(SctpChannelOption.SCTP_NODELAY,
+				true);
 	}
 
 	@Override
